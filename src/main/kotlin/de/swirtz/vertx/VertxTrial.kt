db036880e8d45c3d1ff38f727ed2bc5fb573331f
Simon <simon.wirtzq@gmail.com>
2017-04-07 00:14:06 +0200

    init commit

---
+++ src/main/kotlin/de/swirtz/vertx/VertxTrial.kt
@@ -0,0 +1,16 @@
+package de.swirtz.vertx
+
+import io.vertx.core.*
+
+
+fun main(args: Array<String>) {
+    val vertx = Vertx.vertx()
+
+    var timeCounter = 0;
+    vertx.setPeriodic(4000, { timeCounter+=1; println("$timeCounter: Hello, I am your first Vertx Timer")})
+
+    Thread.sleep(2000)
+
+    vertx.createHttpServer().requestHandler { e -> e.response().end("answer from kotlin vertx app!") }.listen(8000)
+}
+
