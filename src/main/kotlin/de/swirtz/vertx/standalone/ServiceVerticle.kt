b7e9bd01877103cd3cb7dbe2ba7bb29725528705
SIM <simon.wirtz@n-design.de>
2017-04-28 09:23:01 +0200

    kotlin version at 1.1.2; playing around with blocking; added error handling to webVerticle

--- src/main/kotlin/de/swirtz/vertx/standalone/ServiceVerticle.kt
+++ src/main/kotlin/de/swirtz/vertx/standalone/ServiceVerticle.kt
@@ -9,18 +9,30 @@ import org.slf4j.LoggerFactory
  * @author: simon-wirtz
  */
 
-class ServiceVerticle : AbstractVerticle(){
-    companion object {
+class ServiceVerticle : AbstractVerticle() {
+    companion object {  
         val LOG = LoggerFactory.getLogger(ServiceVerticle::class.java)
     }
 
     override fun start() {
-        vertx.eventBus().consumer<JsonObject>(WebVerticle.ACTION, {msg->
+        vertx.eventBus().consumer<JsonObject>(WebVerticle.ACTION, { msg ->
             LOG.debug("Message ${msg.body()} received!")
             val serviceResp = msg.body()
-            serviceResp.put("serviceVerticleResp","answering ACTION event")
-            LOG.debug("Reply Message $serviceResp")
-            msg.reply(serviceResp)
+
+            //some blocking
+            vertx.executeBlocking<Any>({ future ->
+                Thread.sleep(20_000)
+                future.complete("Thread finished")
+            }, false, { res ->
+                LOG.info("Blocking has finished: ${res.result()}")
+                serviceResp.put("serviceVerticleResp", "answering ACTION event from blocking")
+                LOG.debug("Reply Message from blocking $serviceResp")
+                msg.reply(serviceResp)
+            })
+
+//            serviceResp.put("serviceVerticleResp","answering ACTION event")
+//            LOG.debug("Reply Message $serviceResp")
+//            msg.reply(serviceResp)
         })
     }
 }
--- src/main/kotlin/de/swirtz/vertx/standalone/WebVerticle.kt
+++ src/main/kotlin/de/swirtz/vertx/standalone/WebVerticle.kt
@@ -5,6 +5,7 @@ import io.vertx.core.AsyncResult
 import io.vertx.core.eventbus.Message
 import io.vertx.core.json.JsonObject
 import org.slf4j.LoggerFactory
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * Created on 07.04.2017.
@@ -15,17 +16,24 @@ class WebVerticle : AbstractVerticle() {
     companion object {
         val ACTION = "ACTION"
         val LOG = LoggerFactory.getLogger(WebVerticle::class.java)
+        val reqCount = AtomicInteger(0)
     }
 
     override fun start() {
         vertx.createHttpServer().requestHandler { req ->
             LOG.debug("Got request: ${req.host()}")
-            val serviceReq = JsonObject().put("request", "request json from WebVerticle").put("query", req.query())
+            val reqNum = reqCount.incrementAndGet();
+            val serviceReq = JsonObject().put("request", "$reqNum. request json from WebVerticle").put("query", req.query())
             LOG.debug("send request: $serviceReq")
             vertx.eventBus().send(ACTION, serviceReq, { reply: AsyncResult<Message<Any>> ->
-                val replyBody = reply.result()?.body() as JsonObject
-                LOG.debug("Got Reply from event consumer: $replyBody")
-                req.response().putHeader("Content-Type", "application/json").end(replyBody.encodePrettily())
+                if (reply.failed()) {
+                    LOG.error("$reqNum. FAILED! ${reply.cause()}")
+                    req.response().putHeader("Content-Type", "application/json").end("{\"error\": \" ${reply.cause()}: error returned by ServiceVerticle!\"}")
+                } else {
+                    val replyBody = reply.result()?.body() as? JsonObject
+                    LOG.debug("$reqNum. Got Reply from event consumer: $replyBody")
+                    req.response().putHeader("Content-Type", "application/json").end(replyBody?.encodePrettily() ?: "{\"error\": \"null returned by ServiceVerticle!\"}")
+                }
             })
         }.listen(8181)
     }
