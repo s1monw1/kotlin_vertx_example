ed0ab68baf486dab04425c1ffb3448ceeefd07e4
SIM <simon.wirtz@n-design.de>
2017-04-28 15:46:23 +0200

    constants created

---
+++ src/main/kotlin/de/swirtz/vertx/standalone/webserver/Constants.kt
@@ -0,0 +1,9 @@
+package de.swirtz.vertx.standalone.webserver
+
+/**
+ * Created on 28.04.2017.
+ * @author: simon-wirtz
+ */
+
+val ACTION_WEB_REQ_RECEIVED = "webservice.request.received"
+val WEB_SRV_PORT = 8181
